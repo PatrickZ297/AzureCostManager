ce148a34dabb09f7e149dc7eded85b801053770d
PatrickZ297 <patrickzdebor18@gmail.com>
2026-05-19 14:02:55 +0200

    feat: initial project structure with folders

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureCostManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
